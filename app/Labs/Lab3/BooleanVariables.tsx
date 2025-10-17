7b20661be89e09750b2da83b7b469198ec3e8e51
Lucius Manzi <manzi.l@northeastern.edu>
2025-10-17 16:32:40 -0400

    boolean variables

--- app/Labs/Lab3/BooleanVariables.tsx
+++ app/Labs/Lab3/BooleanVariables.tsx
@@ -1,12 +1,12 @@
 export default function BooleanVariables() {
     const numberVariable = 123
-    let floatingPointNumber = 234.345;
+    const floatingPointNumber = 234.345;
     const true1 = true, false1 = false;
     const false2 = true1 && false1;
     const true2 = true1 || false1;
     const true3 = !false2;
-    const true4 = numberVariable === 123; // always use === not ==
-    const true5 = floatingPointNumber !== 321.432;
+    const true4 = numberVariable === 123; 
+    // const true5 = floatingPointNumber !== 321.432;
     const false3 = numberVariable < 100;
     return (
     <div id="wd-boolean-variables">
@@ -17,7 +17,7 @@ export default function BooleanVariables() {
         true2     = {true2 + ""}     <br />
         true3     = {true3 + ""}     <br />
         true4     = {true4 + ""}     <br />
-        true5     = {true5 + ""}     <br />
+        {/* true5     = {true5 + ""}     <br /> */}
         false3    = {false3 + ""}    <hr />
     </div>
     );

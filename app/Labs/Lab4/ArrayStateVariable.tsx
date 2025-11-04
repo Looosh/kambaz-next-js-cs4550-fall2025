941c393732c9d2cd560de6a73d6b2ac1cf6263b4
Lucius Manzi <manzi.l@northeastern.edu>
2025-11-04 18:06:23 -0500

    Redux Examples debug

--- app/Labs/Lab4/ArrayStateVariable.tsx
+++ app/Labs/Lab4/ArrayStateVariable.tsx
@@ -1,41 +1,23 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
-import { RootState } from "./store";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
+// import { RootState } from "./store";
 
 export default function ArrayStateVariable() {
-  const { todos } = useSelector((state: RootState) => state.todosReducer);
-
-  const [array, setArray] = useState<number[]>([1, 2, 3, 4, 5]);
-
-  const addElement = () => {
-    setArray([...array, Math.floor(Math.random() * 100)]);
-  };
-
-  const deleteElement = (index: number) => {
-    setArray(array.filter((_, i) => i !== index));
-  };
-
-  return (
-    <div id="wd-array-state-variables">
-      <h2>Array State Variable</h2>
-      <button onClick={addElement}>Add Element</button>
-      <ul>
-        {array.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => deleteElement(index)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-      <hr />
-
-      <ListGroup>
-        {todos.map((todo: { id: string; title: string }) => (
-          <ListGroupItem key={todo.id}>{todo.title}</ListGroupItem>
-        ))}
-      </ListGroup>
-      <hr />
-    </div>
-  );
-}
+ const [array, setArray] = useState([1, 2, 3, 4, 5]);
+ const addElement = () => {
+   setArray([...array, Math.floor(Math.random() * 100)]);
+ };
+const deleteElement = (index: number) => {
+   setArray(array.filter((item, i) => i !== index));
+ };
+ return (
+  <div id="wd-array-state-variables">
+   <h2>Array State Variable</h2>
+   <button onClick={addElement}>Add Element</button>
+   <ul>
+    {array.map((item, index) => (
+     <li key={index}> {item}
+      <button onClick={() => deleteElement(index)}>
+       Delete</button>
+     </li>))}
+   </ul><hr/></div>);}

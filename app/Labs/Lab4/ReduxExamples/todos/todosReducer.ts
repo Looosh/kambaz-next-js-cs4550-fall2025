83962f7db4d1895e7242de2d66295a7918bb1c1c
Lucius Manzi <manzi.l@northeastern.edu>
2025-11-04 16:55:36 -0500

    Redux Examples debug

--- app/Labs/Lab4/ReduxExamples/todos/todosReducer.ts
+++ app/Labs/Lab4/ReduxExamples/todos/todosReducer.ts
@@ -1,46 +1,38 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-
-export type Todo = {
-  id?: string;
-  title: string;
-};
-
-type TodosState = {
-  todos: Todo[];
-  todo: Todo;
-};
-
-const initialState: TodosState = {
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
   todos: [
     { id: "1", title: "Learn React" },
     { id: "2", title: "Learn Node" },
   ],
   todo: { title: "Learn Mongo" },
 };
-
 const todosSlice = createSlice({
   name: "todos",
   initialState,
   reducers: {
-    addTodo: (state, action: PayloadAction<Todo>) => {
-      const newTodo = { ...action.payload, id: new Date().getTime().toString() };
-      state.todos.push(newTodo);
+    addTodo: (state, action) => {
+      const newTodos = [
+          ...state.todos,
+        { ...action.payload, id: new Date().getTime().toString() },
+      ];
+      state.todos = newTodos;
       state.todo = { title: "" };
     },
-    deleteTodo: (state, action: PayloadAction<string>) => {
-      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    deleteTodo: (state, action) => {
+      const newTodos = state.todos.filter((todo) => todo.id !== action.payload);
+      state.todos = newTodos;
     },
-    updateTodo: (state, action: PayloadAction<Todo>) => {
-      state.todos = state.todos.map((item) =>
+    updateTodo: (state, action) => {
+      const newTodos = state.todos.map((item) =>
         item.id === action.payload.id ? action.payload : item
       );
+      state.todos = newTodos;
       state.todo = { title: "" };
     },
-    setTodo: (state, action: PayloadAction<Todo>) => {
+    setTodo: (state, action) => {
       state.todo = action.payload;
     },
   },
 });
-
 export const { addTodo, deleteTodo, updateTodo, setTodo } = todosSlice.actions;
 export default todosSlice.reducer;
--- app/Labs/Lab4/store.ts
+++ app/Labs/Lab4/store.ts
@@ -3,7 +3,6 @@ import helloReducer from "./ReduxExamples/HelloRedux/helloReducer";
 import counterReducer from "./ReduxExamples/CounterRedux/counterReducer";
 import addReducer from "./ReduxExamples/AddRedux/addReducer";
 import todosReducer from "./ReduxExamples/todos/todosReducer";
-
 const store = configureStore({
   reducer: {
     helloReducer,
@@ -12,6 +11,5 @@ const store = configureStore({
     todosReducer,
   },
 });
-
 export type RootState = ReturnType<typeof store.getState>;
 export default store;

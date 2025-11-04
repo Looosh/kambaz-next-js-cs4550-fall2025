0f1159bf41d2f117e68d0461f333249c8b3c0af4
Lucius Manzi <manzi.l@northeastern.edu>
2025-11-04 00:03:39 -0500

    todolist fix

--- app/Labs/Lab4/store.ts
+++ app/Labs/Lab4/store.ts
@@ -1,16 +1,21 @@
+// app/Labs/Lab4/store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "./ReduxExamples/HelloRedux/helloReducer";
 import counterReducer from "./ReduxExamples/CounterRedux/counterReducer";
-import addReducer     from "./ReduxExamples/AddRedux/addReducer";
+import addReducer from "./ReduxExamples/AddRedux/addReducer";
 import todosReducer from "./ReduxExamples/todos/todosReducer";
 
-
-
 const store = configureStore({
-  reducer: { 
+  reducer: {
     helloReducer,
     counterReducer,
     addReducer,
     todosReducer,
-   }});
+  },
+});
+
+// TypeScript: extract RootState and AppDispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
 export default store;

a1124728e4e7277b484e57dfa09ed11d63438471
Lucius Manzi <manzi.l@northeastern.edu>
2025-11-13 18:09:19 -0500

    Lab5 WorkingWithArrays

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -2,7 +2,7 @@ import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
-
+import WorkingWithArrays from "./WorkingWithArrays";
 
 
 
@@ -20,5 +20,6 @@ export default function Lab5() {
       <PathParameters />
       <QueryParameters />
       <WorkingWithObjects />
+      <WorkingWithArrays />
     </div>
 );}

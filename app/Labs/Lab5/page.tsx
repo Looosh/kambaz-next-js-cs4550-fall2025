52ca51e1c90a81b4cf8d9064ce0e43e80755456b
Lucius Manzi <manzi.l@northeastern.edu>
2025-11-13 15:22:55 -0500

    Lab5 PathPArams

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -1,14 +1,12 @@
-import EnvironmentVariables from "./EnvironmentVariables";
 export default function Lab5() {
   return (
     <div id="wd-lab5">
       <h2>Lab 5</h2>
       <div className="list-group">
-        <a href="http://localhost:4000/lab5/welcome"          
+        <a href="http://localhost:3000/lab5/welcome"          
            className="list-group-item">
            Welcome
         </a>
       </div><hr/>
-      <EnvironmentVariables />
     </div>
 );}

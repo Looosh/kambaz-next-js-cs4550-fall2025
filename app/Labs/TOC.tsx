6e3f3469bdccc41ace306ac1870653b8735ae4dc
Lucius Manzi <manzi.l@northeastern.edu>
2025-11-14 14:54:48 -0500

    TOC additions

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -28,6 +28,15 @@ export default function TOC() {
        <NavLink href="/" as={Link}>
          Kambaz </NavLink> </NavItem>
      <NavItem>
-       <NavLink href="https://github.com/jannunzi">My GitHub</NavLink></NavItem>
+       <NavLink href="https://github.com/Looosh">My GitHub</NavLink>
+      </NavItem>
+
+      <NavItem>
+       <NavLink href="https://github.com/Looosh/kambaz-node-server-app">My GitHub Server</NavLink>
+      </NavItem>
+
+      <NavItem>
+       <NavLink href="https://kambaz-node-server-app-4biq.onrender.com">Render</NavLink>
+      </NavItem>
    </Nav>
  );}

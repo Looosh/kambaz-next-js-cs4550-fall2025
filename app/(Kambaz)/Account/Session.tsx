8df02758c8f1913fc5b6c459659931001d3d6b39
Lucius Manzi <manzi.l@northeastern.edu>
2025-11-14 03:43:51 -0500

    Kambaz5 server sessions any changes

--- app/(Kambaz)/Account/Session.tsx
+++ app/(Kambaz)/Account/Session.tsx
@@ -1,23 +1,33 @@
 import * as client from "./client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, ReactNode } from "react";
 import { setCurrentUser } from "./reducer";
 import { useDispatch } from "react-redux";
-export default function Session({ children }: { children: any }) {
+
+interface SessionProps {
+  children: ReactNode; 
+}
+
+export default function Session({ children }: SessionProps) {
   const [pending, setPending] = useState(true);
   const dispatch = useDispatch();
+
   const fetchProfile = async () => {
     try {
       const currentUser = await client.profile();
       dispatch(setCurrentUser(currentUser));
-    } catch (err: any) {
+    } catch (err: unknown) { 
       console.error(err);
     }
     setPending(false);
   };
+
   useEffect(() => {
     fetchProfile();
   }, []);
+
   if (!pending) {
-    return children;
+    return <>{children}</>;
   }
+
+  return <div>Loading...</div>;
 }

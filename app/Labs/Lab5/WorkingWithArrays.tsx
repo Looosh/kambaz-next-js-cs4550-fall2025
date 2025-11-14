65a0064949d8d87788f2f61343ac7a354ca84d86
Lucius Manzi <manzi.l@northeastern.edu>
2025-11-14 03:39:29 -0500

    Kambaz5 server sessions

--- app/Labs/Lab5/WorkingWithArrays.tsx
+++ app/Labs/Lab5/WorkingWithArrays.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { FormCheck, FormControl } from "react-bootstrap";
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export default function WorkingWithArrays() {
+  
   const API = `${HTTP_SERVER}/lab5/todos`;
   const [todo, setTodo] = useState({
     id: "1",
